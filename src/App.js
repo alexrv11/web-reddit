a1ea0192e85c1c337cb0b2432473f3b99d807413
Alex Ventura <alex.rv11@gmail.com>
2019-09-09 09:12:41 -0300

    implament home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+
+import { Layout, Divider, Pagination, Row, Col, Typography } from 'antd';
+import CardReddit from './components/card'
+
+const { Header, Footer, Content } = Layout;
+const { Title } = Typography
+
+const post1 = {
+  image:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+  icon_avatar:"https://i.redd.it/award_images/t5_2qh0u/bwpkktei53831_Superspecialaward.png",
+  title: "title post",
+  description: "description post ..."
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+      <Title id="reddit-title" level={2}>Reddit Top</Title>
+      </Header>
+      <Content>
+        <Row type="flex" justify="center" gutter={24}>
+          <Col span={6}><CardReddit post={ post1 }/></Col>
+          <Col span={6}><CardReddit post={ post1 }/></Col>
+          <Col span={6}><CardReddit post={ post1 }/></Col>
+        </Row>
+        <Divider />
+        <Row type="flex" justify="center">
+          <Pagination defaultCurrent={1} total={50} />
+        </Row>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
---
+++ src/components/card.jsx
@@ -0,0 +1,38 @@
+import './card.css'
+import React from 'react'
+import {Card, Icon, Avatar } from 'antd'
+
+const { Meta } = Card;
+
+const CardReddit = (props) => {
+    const handleClick = () => {
+      console.log("Hello")
+    }
+
+    const { image, title, description, icon_avatar } = props.post
+
+    return (
+      <Card
+        style={{ width: 300 }}
+        cover={
+          <img
+            alt="example"
+            src={image}
+          />
+        }
+        actions={[
+          <Icon type="delete" theme="filled" />,
+        ]}
+      >
+        <Meta
+          className="reddit-card"
+          avatar={<Avatar src={ icon_avatar } />}
+          title={ title }
+          description={ description }
+          onClick={ handleClick }
+        />
+      </Card>
+    );
+}
+
+export default CardReddit;
